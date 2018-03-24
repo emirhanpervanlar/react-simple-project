f01f34abc5b15496c088bee87f39e6c4b49348d0
Emirhan <pervanlaremirhan@gmail.com>
2018-03-24 16:10:59 +0300

    children

---
+++ client/components/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <h3>Home page</h3>
+            </div>
+        )
+    }
+}
+export default Home;
